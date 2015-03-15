87e7825439611818bd7164a06d90c7526f19689d
John C. Bland II <johncblandii@gmail.com>
2015-03-15 02:39:44 -0500

    Ported the post model and controller to concerns
    This helps with overriding to add functionality.

---
+++ app/controllers/concerns/blogit/postable.rb
@@ -0,0 +1,82 @@
+module Blogit::Postable
+  extend ActiveSupport::Concern
+
+  included do
+    # If using Blogit's Create, Update and Destroy actions AND ping_search_engines is
+    # set, call ping_search_engines after these requests
+    if Blogit.configuration.include_admin_actions
+      after_filter :ping_search_engines, only: [:create, :update, :destroy], :if => lambda { Blogit.configuration.ping_search_engines }
+    end
+
+    # Raise a 404 error if the admin actions aren't to be included
+    # We can't use blogit_conf here because it sometimes raises NoMethodError in main app's routes
+    unless Blogit.configuration.include_admin_actions
+      before_filter :raise_404, except: [:index, :show]
+    end
+
+    blogit_authenticate(except: [:index, :show])
+  end
+
+  def index
+    @posts = Blogit::Post.order('created_at DESC').page(params[:page]).per(Blogit.configuration.posts_per_page)
+  end
+
+  def show
+    @post = Blogit::Post.find(params[:id])
+  end
+
+  def new
+    @post = current_blogger.blog_posts.new(params.key(:post) || {})
+  end
+
+  def edit
+    @post = current_blogger.blog_posts.find(params[:id])
+  end
+
+  def create
+    @post = current_blogger.blog_posts.new(valid_params)
+    if @post.save
+      redirect_to @post, notice: t(:blog_post_was_successfully_created, scope: 'blogit.posts')
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @post = current_blogger.blog_posts.find(params[:id])
+    if @post.update_attributes(valid_params)
+      redirect_to @post, notice: t(:blog_post_was_successfully_updated, scope: 'blogit.posts')
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @post = current_blogger.blog_posts.find(params[:id])
+    @post.destroy
+    redirect_to posts_url, notice: t(:blog_post_was_successfully_destroyed, scope: 'blogit.posts')
+  end
+
+  protected
+  def valid_params
+    params.require(:post).permit(:title, :excerpt, :body, :blogger_type, :blogger_id, :tag_list)
+  end
+
+  def raise_404
+    # Don't include admin actions if include_admin_actions is false
+    render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false
+  end
+
+  # @See the Pingr gem for more info https://github.com/KatanaCode/pingr
+  def ping_search_engines
+    case blogit_conf.ping_search_engines
+    when Array
+      search_engines = blogit_conf.ping_search_engines
+    when true
+      search_engines = Pingr::SUPPORTED_SEARCH_ENGINES
+    end
+    for search_engine in search_engines
+      Pingr::Request.new(search_engine, posts_url(format: :xml)).ping
+    end
+  end
+end
---
+++ app/models/concerns/blogit/blog_post.rb
@@ -0,0 +1,74 @@
+module Blogit::BlogPost
+  extend ActiveSupport::Concern
+
+  included do
+    require "kaminari"
+
+    self.table_name = "blog_posts"
+
+    acts_as_taggable
+
+    # ================
+    # = Associations =
+    # ================
+
+    belongs_to :blogger, :polymorphic => true
+
+    has_many :comments, :class_name => "Blogit::Comment"
+
+    # ===============
+    # = Validations =
+    # ===============
+
+    validates :title, presence: true, length: { minimum: 10, maximum: 66 }
+    validates :body,  presence: true, length: { minimum: 10 }
+    validates :blogger_id, presence: true
+
+    # ==========
+    # = Scopes =
+    # ==========
+
+    # Returns the blog posts paginated for the index page
+    # @scope class
+    scope :for_index, lambda { |page_no = 1| order("created_at DESC").page(page_no) }
+  end
+
+  def comments
+    check_comments_config
+
+    super()
+  end
+
+  def comments=(value)
+    check_comments_config
+
+    super(value)
+  end
+
+  # ====================
+  # = Instance Methods =
+  # ====================
+
+  def to_param
+    "#{id}-#{title.parameterize}"
+  end
+
+  # If there's a current blogger and the display name method is set, returns the blogger's display name
+  # Otherwise, returns an empty string
+  def blogger_display_name
+    if self.blogger and !self.blogger.respond_to?(Blogit.configuration.blogger_display_name_method)
+      raise ConfigurationError,
+      "#{self.blogger.class}##{Blogit.configuration.blogger_display_name_method} is not defined"
+    elsif self.blogger.nil?
+      ""
+    else
+      self.blogger.send Blogit.configuration.blogger_display_name_method
+    end
+  end
+
+  private
+
+  def check_comments_config
+    raise RuntimeError.new("Posts only allow active record comments (check blogit configuration)") unless Blogit.configuration.include_comments == :active_record
+  end
+end
